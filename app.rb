989bd12583699983794ddc4faa8f5b8fe5bbcb62
Drew Thompson <drewallynthompson@gmail.com>
2020-11-01 00:47:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ class App < Sinatra::Base
     @number = params[:number]
     @phrase = params[:phrase]
     String.new(@number, "{@phrase}")
+  end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @word1 = params[:word1]
