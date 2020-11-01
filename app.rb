63269de6186d7bfaa16ea2ab5205e5279cae3cda
Drew Thompson <drewallynthompson@gmail.com>
2020-11-01 00:42:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,8 +8,15 @@ class App < Sinatra::Base
   
   get '/square/:number' do
     @number = params[:number]
-    "#{@number.to_i**2}.to_s"
+    "#{@number.to_i**2}"
   end
   
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{word1} #{word2} #{word3} #{word4} #{word5}"
 
 end
